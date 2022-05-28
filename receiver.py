61df71b0357cfbe1c8d3d8a2c392cfa7b67fc6bd
Tom Grant <thomas.grant.mail@gmail.com>
2022-05-28 18:08:09 +0100

    Better receiver CLI for known files

--- receiver.py
+++ receiver.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from OFDM import demodulate_signal
 from config import SAMPLE_RATE, MAX_RECORDING_DURATION, RECORDING_OUTPUT_DIR
 from common import (
@@ -5,7 +6,11 @@ from common import (
     set_audio_device_or_warn,
     finalize_argparse_for_sounddevice,
 )
-from synchronization_estimation import crop_signal_into_parts, estimate_channel_coefficients
+from error_stats import bit_error
+from synchronization_estimation import (
+    crop_signal_into_parts,
+    estimate_channel_coefficients,
+)
 
 
 import numpy as np
@@ -24,7 +29,6 @@ def receive_signal(signal):
 
     demodulated_signal = demodulate_signal(channel_coefficients_start, ofdm_signal)
 
-    open('output', 'wb').write(demodulated_signal)
     return demodulated_signal
 
 
@@ -46,14 +50,24 @@ def record_until_enter_key():
 if __name__ == "__main__":
     parser = ArgumentParser(description="OFDM receiver")
     parser.add_argument("file", nargs="?", help="Numpy waveform file")
+    parser.add_argument("--expected_output", help="Expected transmission")
     args = finalize_argparse_for_sounddevice(parser)
 
     if args.file is not None:
         signal_from_file = np.load(args.file)
-        receive_signal(signal_from_file)
-        exit(0)
+    else:
+        set_audio_device_or_warn(args)
+        recorded_signal = record_until_enter_key()
 
-    set_audio_device_or_warn(args)
+    demodulated_file = receive_signal(signal_from_file)
 
-    recorded_signal = record_until_enter_key()
-    receive_signal(recorded_signal)
+    if args.expected_output is not None:
+        with open(args.expected_output, "rb") as expected_file:
+            expected_bytes = expected_file.read()
+
+        demodulated_file = demodulated_file[: len(expected_bytes)]
+        print(
+            f"[INFO] Bit error of received file: {bit_error(demodulated_file, expected_bytes)}"
+        )
+
+    open("output", "wb").write(demodulated_file)
