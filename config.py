FIX: Off-by-one errors for standard compliance

config.py
@@ -32,7 +32,7 @@ OFDM_CYCLIC_PREFIX_LENGTH = 1 << 9
 OFDM_SYMBOL_LENGTH = OFDM_BODY_LENGTH + OFDM_CYCLIC_PREFIX_LENGTH
 OFDM_DATA_INDEX_RANGE = { # following python standard range convention
     "min": get_index_of_frequency(1000) + 1,
-    "max": get_index_of_frequency(10_000),
+    "max": get_index_of_frequency(10_000) + 1,
 }
 MAX_NUMBER_OF_SYMBOLS_IN_FRAME = 128
 KNOWN_OFDM_BLOCK_FFT = np.load('known_ofdm_symbol.npy')

ldcp_tools.py
@@ -23,7 +23,10 @@ def encode_bytes(data: bytes):
 
 def decode_from_llr(llr: np.ndarray):
     excess_llr_blocks = llr.size % LDPC_CODER.N
-    cropped_llr = llr[:-excess_llr_blocks]
+
+    cropped_llr = llr
+    if (excess_llr_blocks != 0):
+        cropped_llr = llr[:-excess_llr_blocks]
 
     llr_blocks_for_jossy = np.split(cropped_llr, cropped_llr.size // LDPC_CODER.N)
 