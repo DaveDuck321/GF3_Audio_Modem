703667b54fd076092a0183006f8671be62ce9da0
Thomas Grant <thomas.grant.duck321@gmail.com>
2022-06-03 11:46:08 +0100

    Update to little endian to meet standard

--- metadata.py
+++ metadata.py
@@ -5,7 +5,7 @@ def generate_bytes_for_transmission(filename, content: bytes):
     length_of_file = len(content)
 
     data = bytearray()
-    data.extend(length_of_file.to_bytes(4, byteorder="big"))
+    data.extend(length_of_file.to_bytes(4, byteorder="little"))
     data.extend(filename.encode())
     data.extend(b"\0")
     data.extend(content)
@@ -14,7 +14,7 @@ def generate_bytes_for_transmission(filename, content: bytes):
 
 
 def decode_received_file(data: bytes):
-    length = int.from_bytes(data[:4], byteorder="big")
+    length = int.from_bytes(data[:4], byteorder="little")
     filename, padded_content = data[4:].split(b"\0", 1)
 
     sanitized_filename = "".join(
